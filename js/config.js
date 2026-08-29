9e8206450ebcab7a164cb83d31440aed9167df20
MojiMacha <mothegaming.work@gmail.com>
2026-08-29 11:54:07 +0700

    config.js

--- js/config.js
+++ js/config.js
@@ -1,15 +1,8 @@
 const CONFIG = {
     STORAGE_KEY: 'DELIVERY_PRO_DB',
+    STAFF_PIN: '001456', // รหัสผ่าน PIN สำหรับพนักงาน
     RISK_THRESHOLDS: {
         HIGH: 0.40,   // ตีกลับตั้งแต่ 40% ขึ้นไป = ความเสี่ยงสูง
         MEDIUM: 0.20  // ตีกลับตั้งแต่ 20% ขึ้นไป = ความเสี่ยงปานกลาง
-    }
-};
-const CONFIG = {
-    STORAGE_KEY: 'customer_risk_data',
-    STAFF_PIN: '001456', // รหัสผ่าน PIN สำหรับพนักงาน
-    RISK_THRESHOLDS: {
-        HIGH: 0.4,
-        MEDIUM: 0.2
     }
 };
