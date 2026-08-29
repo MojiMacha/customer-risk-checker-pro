0fae4ea4a09c77849d89cdb01a0f17f09a1a9e30
MojiMacha <mothegaming.work@gmail.com>
2026-08-29 10:59:20 +0700

    config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    STORAGE_KEY: 'DELIVERY_PRO_DB',
+    RISK_THRESHOLDS: {
+        HIGH: 0.40,   // ตีกลับตั้งแต่ 40% ขึ้นไป = ความเสี่ยงสูง
+        MEDIUM: 0.20  // ตีกลับตั้งแต่ 20% ขึ้นไป = ความเสี่ยงปานกลาง
+    }
+};
