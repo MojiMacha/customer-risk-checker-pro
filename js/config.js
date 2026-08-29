b30373a73268d8c18d30f93a224aa3088e6e66ce
MojiMacha <mothegaming.work@gmail.com>
2026-08-29 11:26:48 +0700

    config.js

--- js/config.js
+++ js/config.js
@@ -5,3 +5,11 @@ const CONFIG = {
         MEDIUM: 0.20  // ตีกลับตั้งแต่ 20% ขึ้นไป = ความเสี่ยงปานกลาง
     }
 };
+const CONFIG = {
+    STORAGE_KEY: 'customer_risk_data',
+    STAFF_PIN: '001456', // รหัสผ่าน PIN สำหรับพนักงาน
+    RISK_THRESHOLDS: {
+        HIGH: 0.4,
+        MEDIUM: 0.2
+    }
+};
