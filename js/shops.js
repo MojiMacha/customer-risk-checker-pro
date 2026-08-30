bb0dda0dfd923bd7b4a66d1457e36106b2d31103
MojiMacha <mothegaming.work@gmail.com>
2026-08-31 00:17:28 +0700

    Update shops.js

--- js/shops.js
+++ js/shops.js
@@ -1,1 +1,30 @@
+const shops = [
 
+    {
+        id: "S001",
+        name: "Dr.PONG",
+        logo: "images/shops/Dr.Pong.jpeg",
+        mall: true,
+        description: "ร้านผลิตภัณฑ์ดูแลผิวและความงาม",
+        rating: 4.9
+    },
+
+    {
+        id: "S002",
+        name: "Fashion Store",
+        logo: "images/shops/shop2.jpg",
+        mall: true,
+        description: "ร้านเสื้อผ้าและแฟชั่น",
+        rating: 4.8
+    },
+
+    {
+        id: "S003",
+        name: "ร้านค้าทั่วไป",
+        logo: "images/shops/shop1.jpg",
+        mall: false,
+        description: "ร้านสินค้าทั่วไป",
+        rating: 4.7
+    }
+
+];
