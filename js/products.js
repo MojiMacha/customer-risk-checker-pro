3ecdcbfb04e8b2ddf7e25e6e902782ef7acdd4c7
MojiMacha <mothegaming.work@gmail.com>
2026-08-31 00:18:38 +0700

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,1 +1,62 @@
+const products = [
 
+    {
+        id: "P001",
+
+        name: "Dr.PONG 28D Smart Whitening Drone Activein Serum",
+
+        image: "images/products/dr.pongSmart28D.jpeg",
+
+        description:
+            "เซรั่มบำรุงผิวหน้าสูตรกระจ่างใส ช่วยลดเลือนจุดด่างดำและรอยสิว",
+
+        price: 599,
+
+        category: "beauty",
+
+        rating: 4.9,
+
+        shopId: "S001"
+    },
+
+
+    {
+        id: "P002",
+
+        name: "เสื้อยืดแฟชั่น",
+
+        image: "images/products/product2.jpg",
+
+        description:
+            "เสื้อยืดแฟชั่น เนื้อผ้านุ่ม ใส่สบาย เหมาะสำหรับใส่ในชีวิตประจำวัน",
+
+        price: 299,
+
+        category: "fashion",
+
+        rating: 4.8,
+
+        shopId: "S002"
+    },
+
+
+    {
+        id: "P003",
+
+        name: "หูฟัง Bluetooth",
+
+        image: "images/products/product3.jpg",
+
+        description:
+            "หูฟังไร้สาย Bluetooth เสียงคมชัด ใช้งานง่าย",
+
+        price: 890,
+
+        category: "electronics",
+
+        rating: 4.7,
+
+        shopId: "S003"
+    }
+
+];
