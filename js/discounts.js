4e71281fc556e8afc1453d0055e70d953cd0e13b
MojiMacha <mothegaming.work@gmail.com>
2026-08-31 13:27:20 +0700

    Update discounts.js

--- js/discounts.js
+++ js/discounts.js
@@ -1,1 +1,23 @@
-
+const discounts = [
+    {
+        code: "WELCOME50",
+        type: "fixed",
+        value: 50,
+        minimum: 300,
+        active: true
+    },
+    {
+        code: "SAVE10",
+        type: "percent",
+        value: 10,
+        minimum: 500,
+        active: true
+    },
+    {
+        code: "FREESHIP",
+        type: "shipping",
+        value: 0,
+        minimum: 300,
+        active: true
+    }
+];
