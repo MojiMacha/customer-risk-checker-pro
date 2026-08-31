7763e17ed711d5311fa71c3d0e820ecd5f11464d
MojiMacha <mothegaming.work@gmail.com>
2026-08-31 21:17:09 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
     DRIVER_PIN: '001456', // 🔑 PIN สำหรับพนักงานส่ง
     ADMIN_PIN:  '999888', // 🔑 PIN สำหรับแอดมินผู้ดูแลระบบ
-    STAFF_PIN:  '001456'  // PIN สำรอง
+    STAFF_PIN:  '333222'  // PIN สำรอง
 };
